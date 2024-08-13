d08130c726e4801e4b261f65bc0a16b22f33c476
Anastasija Samcovic <anastasija.samcovic@myfitworld.net>
2024-08-13 21:48:49 +0200

    feat: delete account

--- src/requests/user/UpdateUserRequest.ts
+++ src/requests/user/UpdateUserRequest.ts
@@ -1,5 +1,4 @@
 export interface UpdateUserRequest {
-    id: number;
     firstName: string;
     lastName: string;
     email: string;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,10 +2,12 @@ import { UpdateUserRequest } from "../requests/user/UpdateUserRequest";
 import { UpdatePasswordRequest } from "../requests/user/UpdatePasswordRequest";
 
 export const updateUser = async (request: UpdateUserRequest) => {
+    const token = sessionStorage.getItem('token');
     const response = await fetch('http://localhost:8080/users/edit-profile', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token,
         },
         body: JSON.stringify(request),
       });
@@ -18,10 +20,12 @@ export const updateUser = async (request: UpdateUserRequest) => {
 }
 
 export const updatePassword = async (request: UpdatePasswordRequest) => {
+    const token = sessionStorage.getItem('token');
     const response = await fetch('http://localhost:8080/users/edit-password', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token,
         },
         body: JSON.stringify(request),
       });
@@ -29,6 +33,25 @@ export const updatePassword = async (request: UpdatePasswordRequest) => {
       return "";
     } else {
         const errorMessage = await response.text();
+        return errorMessage;
+    }
+}
+
+export const deleteAccount = async () => {
+    const token = sessionStorage.getItem('token');
+    const response = await fetch('http://localhost:8080/users/delete-account', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token,
+        }
+      });
+    if(response.ok) {
+      return "";
+    } else {
+        
+        const errorMessage = await response.text();
+        console.log(errorMessage);
         return errorMessage;
     }
 }
